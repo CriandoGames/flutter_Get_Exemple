3bf3f820eaac64b0ce38259bc252046e786f1d85
Carlos Castro <carloscastrogames@gmail.com>
2020-06-09 13:50:14 -0300

    update vers 1

--- lib/screens/Home_view.dart
+++ lib/screens/Home_view.dart
@@ -6,20 +6,47 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Get Corona Virus in word"),
-          centerTitle: true,
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-        ),
-        body: GetBuilder<HomeController>(
+      appBar: AppBar(
+        title: Text("Get Corona Virus in word"),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: Center(
+        child: GetBuilder<HomeController>(
           init: HomeController(),
-          initState: (_){
+          initState: (_) {
             HomeController.to.Result();
           },
           builder: (_) {
-            return  _.data == null ? CircularProgressIndicator() : Text(_.data.global.totalConfirmed.toString());
+            return _.data == null
+                ? CircularProgressIndicator()
+                : Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text("Total de casos no mundo!",
+                          style: Theme.of(context).textTheme.headline5),
+                      Text(
+                        _.data.global.totalConfirmed.toString(),
+                        style: Theme.of(context).textTheme.headline4,
+                      ),
+                      Text("Total de Mortes no Mundo!",
+                          style: Theme.of(context).textTheme.headline5),
+                      Text(
+                        _.data.global.totalDeaths.toString(),
+                        style: Theme.of(context).textTheme.headline4,
+                      ),
+                      Text("Total Recuperados no mundo!",
+                          style: Theme.of(context).textTheme.headline5),
+                      Text(
+                        _.data.global.totalRecovered.toString(),
+                        style: Theme.of(context).textTheme.headline4,
+                      ),
+                    ],
+                  );
           },
-        ));
+        ),
+      ),
+    );
   }
 }
