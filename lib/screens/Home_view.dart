912b37f0580ddfe3dfa5be57479953cc1d6bdc63
Carlos Castro <carloscastrogames@gmail.com>
2020-06-14 11:58:52 -0300

    vers debug 1.0

--- lib/screens/Home_view.dart
+++ lib/screens/Home_view.dart
@@ -6,14 +6,12 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Get Corona Virus in word"),
-        centerTitle: true,
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-      ),
-      body: Center(
-        child: GetBuilder<HomeController>(
+        appBar: AppBar(
+          title: Text("Get Corona Virus in word"),
+          centerTitle: true,
+          elevation: 6,
+        ),
+        body: GetBuilder<HomeController>(
           init: HomeController(),
           initState: (_) {
             HomeController.to.Result();
@@ -21,32 +19,112 @@ class Home extends StatelessWidget {
           builder: (_) {
             return _.data == null
                 ? CircularProgressIndicator()
-                : Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text("Total de casos no mundo!",
-                          style: Theme.of(context).textTheme.headline5),
-                      Text(
-                        _.data.global.totalConfirmed.toString(),
-                        style: Theme.of(context).textTheme.headline4,
-                      ),
-                      Text("Total de Mortes no Mundo!",
-                          style: Theme.of(context).textTheme.headline5),
-                      Text(
-                        _.data.global.totalDeaths.toString(),
-                        style: Theme.of(context).textTheme.headline4,
-                      ),
-                      Text("Total Recuperados no mundo!",
-                          style: Theme.of(context).textTheme.headline5),
-                      Text(
-                        _.data.global.totalRecovered.toString(),
-                        style: Theme.of(context).textTheme.headline4,
-                      ),
-                    ],
+                : GridView.count(
+                    // Create a grid with 2 columns. If you change the scrollDirection to
+                    // horizontal, this produces 2 rows.
+                    crossAxisCount: 2,
+                    // Generate 100 widgets that display their index in the List.
+                    children: List.generate(_.data.countries.length, (index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          margin: EdgeInsets.all(2),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(32),
+                              border: Border.all(width: 0.5)),
+                          width: 50,
+                          height: 50,
+                          child: Column(
+                            children: <Widget>[
+                              Text(
+                                _.data.countries[index].country,
+                                style: Theme.of(context).textTheme.headline6,
+                                textAlign: TextAlign.center,
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Image.network(
+                                "https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Flag_of_Brazil_%281889%E2%80%931960%29.svg/1200px-Flag_of_Brazil_%281889%E2%80%931960%29.svg.png",
+                                height: 40,
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text("Novos Casos: "),
+                                  Text(
+                                    _.data.countries[index].newConfirmed
+                                        .toString(),
+                                    style: TextStyle(color: Colors.orange),
+                                  )
+                                ],
+                              ),
+                              SizedBox(
+                                height: 3,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text("Total de Mordes: "),
+                                  Text(
+                                    _.data.countries[index].totalDeaths
+                                        .toString(),
+                                    style: TextStyle(color: Colors.red),
+                                  )
+                                ],
+                              ),
+                              SizedBox(
+                                height: 1,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text("Recuperados: "),
+                                  Text(
+                                    _.data.countries[index].totalRecovered
+                                        .toString(),
+                                    style: TextStyle(color: Colors.green),
+                                  )
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text("Total de casos: "),
+                                  Text(
+                                    _.data.countries[index].totalConfirmed
+                                        .toString(),
+                                    style: TextStyle(color: Colors.red),
+                                  )
+                                ],
+                              ),
+                              SizedBox(
+                                height: 1,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text("Data: "),
+                                  Text(
+                                    _.data.countries[index].date
+                                        .toString().replaceRange(10,24,'.'),
+                                    style: TextStyle(color: Colors.red),
+                                  )
+                                ],
+                              ),
+                              SizedBox(
+                                height: 1,
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    }),
                   );
           },
-        ),
-      ),
-    );
+        ));
   }
 }
